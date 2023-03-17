cb7ecbc4482eed48a4da1685883aa0f3e0335ac6
SSBakh07 <ssbakh07@gmail.com>
2023-03-18 01:14:26 +0330

    Simple thank you msg try 1

--- cai.py
+++ cai.py
@@ -11,3 +11,9 @@ class CAi(commands.Cog):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("I can do stuff!")
+
+    @client.event
+    async def on_message(message):
+        if "thank you fazuran" in message.content.lower():
+            channel = message.channel
+            await channel.send("You're very welcome <3")
