d29ae67f74cb886d5b3361f1c910591b2da87533
SSBakh07 <ssbakh07@gmail.com>
2023-03-18 01:24:15 +0330

    Message event re-added

--- cai.py
+++ cai.py
@@ -6,6 +6,7 @@ class CAi(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.last_msg = "I saw nothing"
+        self.event_count = 0
 
     @commands.command()
     async def mycom(self, ctx):
@@ -13,12 +14,12 @@ class CAi(commands.Cog):
         # Your code will go here
         await ctx.send("I can do stuff!")
 
-    # @client.event
-    # async def on_message(message):
-    #     if "thank you fazuran" in message.content.lower():
-    #         channel = message.channel
-    #         self.last_msg = message.content
-    #         await channel.send("You're very welcome <3")
+    @client.event
+    async def on_message(self, message):
+        if "thank you fazuran" in message.content.lower():
+            channel = message.channel
+            self.last_msg = message.content
+            await channel.send("You're very welcome <3")
     
     @commands.command()
     async def lastthingyousaw(self, ctx):
